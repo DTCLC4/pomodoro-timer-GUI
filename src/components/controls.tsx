cc3ff7b34b9aad1f4a2c04a4f92e8bda7ef7bf69
dinhthi12 <thi884641@gmail.com>
2024-10-10 14:28:15 +0700

    Build UI timer and add redux-toolkit 🎨

---
+++ src/components/controls.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Controls: React.FC = () => {
+  return (
+    <form className='controls'>
+      <input type='radio'
+        id=''
+        name=''
+      // checked={}
+      // onClick={ }
+      // onChange={ }
+      />
+      <label htmlFor="" className='controls_button'>Pomodoro</label>
+
+      <input type='radio'
+        id=''
+        name=''
+      // checked={}
+      // onClick={ }
+      // onChange={ }
+      />
+      <label htmlFor="" className='controls_button'>Short break</label>
+
+      <input type='radio'
+        id=''
+        name=''
+      // checked={}
+      // onClick={ }
+      // onChange={ }
+      />
+      <label htmlFor="" className='controls_button'>Long break</label>
+    </form>
+  )
+}
+
+export default Controls
